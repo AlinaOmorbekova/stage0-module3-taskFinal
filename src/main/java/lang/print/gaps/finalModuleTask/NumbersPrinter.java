de7bceb5a12a8d69300de1b299ed1b2017bbe815
AlinaOmorbekova <alina.omorbekova@iaau.edu.kg>
2022-07-28 17:27:21 +0600

    final task

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,6 +2,9 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
+        boolean True = number > 0;
+        boolean False = number < 0;
+        System.out.println((True && False) || True);
 
     }
 }
